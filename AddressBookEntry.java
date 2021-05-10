7c15a2ea994e265fd04b56a7340cfefcdcd2ba57
Babakailash <nathkailash2020@gmail.com>
2021-05-11 02:06:44 +0530

    Added:Address Book diamond Project

---
+++ AddressBookEntry.java
@@ -0,0 +1,84 @@
+package demo;
+
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class AddressBookEntry {
+    private String fname;
+    private String lname;
+    private String address;
+    private String city;
+    private String state;
+    private int zipCode;
+    private int mobile;
+    private String email;
+
+    public AddressBookEntry(){
+
+    }
+    public AddressBookEntry(String fname, String lname, String address, String city, String state, int zipCode, int mobile,String email) {
+        this.fname = fname;
+        this.lname = lname;
+        this.address= address;
+        this.city = city;
+        this.state = state;
+        this.zipCode =zipCode;
+        this.mobile= mobile;
+        this.email =email;
+        }
+public String getFname() {
+        return fname;
+    }
+public void setFname(String fname) {
+    this.fname = fname;
+}
+public String getLname() {
+        return lname;
+}
+public void setLname(String lname){
+        this.lname=lname;
+}
+public String getAddress(){
+        return address;
+}
+public void setAddress(String address){
+        this.address=address;
+}
+public String getCity(){
+        return city;
+}
+public void setCity(String city){
+        this.city= city;
+}
+public String getState(){
+        return state;
+}
+public void setState(String state){
+        this.state= state;
+}
+public int getZipCode(){
+        return zipCode;
+}
+public void setZipCode(int zipCode){
+        this.zipCode=zipCode;
+}
+public int getMobile(){
+        return mobile;
+}
+public void setMobile(int mobile){
+        this.mobile=mobile;
+}
+public String getEmail(){
+        return email;
+}
+public void setEmail(String email){
+        this.email=email;
+}
+
+public String toString() {
+        return "Fname: " + fname + ", Lname: " + lname + ",Address: " + address + ", City: " + city + ", State: " + state + ",Zipcode: " + zipCode + ", Mobile: " + mobile + ",Email: " + email;
+}
+    }
+
+
+
---
+++ AddressBookTest.java
@@ -0,0 +1,125 @@
+package demo;
+
+import java.net.SocketOption;
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class AddressBookTest {
+
+    public static void main(String[] args) {
+        System.out.println("**Program Infomation** \nFname" +
+                "-> Is the Name of the Person in Address Book \nLname" +
+                "-> ");
+
+        String input;
+        Scanner in = new Scanner(System.in);
+
+        AddressBook addressBook = new AddressBook();
+
+        boolean stop = false;
+        do {
+            System.out.println("\nMain Menu");
+            System.out.println("1. Add an Entry");
+            System.out.println("2. Delete an Entry");
+            System.out.println("3. View All Entries");
+            System.out.println("4. Update an Entry");
+            System.out.println("5. Exit");
+            System.out.println("Please Enter Choice from 1 to 5: \n");
+            input = (in.next());
+
+
+            switch (input) {
+                case "1":
+                    //create a new entry for the addressbook
+                    AddressBookEntry entry = new AddressBookEntry();
+
+                    System.out.println("***Adding entry in Address Book***");
+
+                    System.out.println("First Name: ");
+                    entry.setFname(in.next());
+
+                    System.out.println("Last Name: ");
+                    entry.setLname(in.next());
+
+                    System.out.println("Address: ");
+                    entry.setAddress(in.next());
+
+                    System.out.println("City: ");
+                    entry.setCity(in.next());
+
+                    System.out.println("State: ");
+                    entry.setState(in.next());
+
+                    System.out.println("ZipCode: ");
+                    entry.setZipCode(in.nextInt());
+
+                    System.out.println("Mobile: ");
+                    entry.setMobile(in.nextInt());
+
+                    System.out.println("Email: ");
+                    entry.setEmail(in.next());
+
+                    //add our new entry to the address Book
+                    addressBook.add(entry);
+
+                    System.out.println("Added a New Entry. ");
+                    break;
+
+                case "2":
+                    System.out.println("Enter the index of the Entry, which you wand To Delete: ");
+                    //delete the entry at the given index
+                    addressBook.delete(in.nextInt());
+
+                    break;
+
+                case "3":
+                    System.out.println("Your addressbook contains the following entries: ");
+                    //get array of all entries
+                    AddressBookEntry[] listOfEntries = addressBook.viewAll();
+
+                    //for every entry in the array
+                    for (int i=0; i< listOfEntries.length; i++){
+                        System.out.println(listOfEntries[i].toString());
+
+                }
+                    break;
+
+                case "4":
+                    System.out.println("Enter the index of the entry, which you want to update: ");
+                    //get entry at the given index
+                    AddressBookEntry entryToUpdate =  addressBook.get(in.nextInt());
+
+                    System.out.println("First Name (current: " + entryToUpdate.getFname() + "):");
+                    entryToUpdate.setFname(in.next());
+
+                    System.out.println("Last Name (current: " + entryToUpdate.getLname() + "):");
+                    entryToUpdate.setLname(in.next());
+
+                    System.out.println("Address (current: " + entryToUpdate.getAddress() + "):");
+                    entryToUpdate.setAddress(in.next());
+
+                    System.out.println("City (current: " + entryToUpdate.getCity() + "):");
+                    entryToUpdate.setCity(in.next());
+
+                    System.out.println("State (current: " + entryToUpdate.getState() + "):");
+                    entryToUpdate.setState(in.next());
+
+                    System.out.println("Zip Code (current: " + entryToUpdate.getZipCode() + "):");
+                    entryToUpdate.setZipCode(in.nextInt());
+
+                    System.out.println("Mobile Number (current: " + entryToUpdate.getMobile() + "):");
+                    entryToUpdate.setMobile(in.nextInt());
+
+                    System.out.println("Email (current: " + entryToUpdate.getEmail() + "):");
+                    entryToUpdate.setEmail(in.next());
+
+                    break;
+
+                default:
+                    break;
+            }
+            //execute while Stop is False
+        }      while (!input.equals("5"));
+            System.out.println("***Kailashnath Vishwakarma..***");
+    }
+}
